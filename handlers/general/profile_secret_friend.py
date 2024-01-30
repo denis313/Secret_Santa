ef2e8cb3df151ce96197e9bc0d9664f810d9cd63
Denis Evtushok <evtushokdenis2004@gmail.com>
2024-01-30 12:15:25 +0300

    gift_details рефакторинг

--- handlers/general/profile_secret_friend.py
+++ handlers/general/profile_secret_friend.py
@@ -1,4 +1,6 @@
 from aiogram import F, Router, Bot
+from aiogram.filters import StateFilter
+from aiogram.fsm.state import default_state
 from aiogram.types import Message, CallbackQuery
 
 from LEXICON.lexicon import LEXICON_keyboard, LEXICON_user, LEXICON
@@ -8,7 +10,8 @@ from filters.filter import IsPrivate
 from handlers.general.message_profile import profile
 from keyboards.callback_data_classes import CallbackFactory
 from keyboards.general_keyboards import keyboard_friend, keyboard_user, keyboard_creator, keyboard_list
-from services.parsing import read_json
+
+# from services.parsing import read_json
 
 router = Router()
 router.message.filter(IsPrivate())
@@ -16,12 +19,12 @@ dsn = DATABASE_URL
 db_manager = DatabaseManager(dsn=dsn)
 
 
-@router.message(F.text == LEXICON_keyboard["button"][4])
+@router.message(F.text == LEXICON_keyboard["button"][4], StateFilter(default_state))
 async def secret_friend(message: Message):
     await message.answer(LEXICON_user["secret_friend"], reply_markup=keyboard_friend.as_markup(resize_keyboard=True))
 
 
-@router.message(F.text == LEXICON_keyboard["friend_button"][0])
+@router.message(F.text == LEXICON_keyboard["friend_button"][0], StateFilter(default_state))
 async def profile_secret_friend(message: Message, bot: Bot):
     id_user = message.from_user.id
     id_friend = await db_manager.get_user_by_id(user_id=id_user)
@@ -29,19 +32,18 @@ async def profile_secret_friend(message: Message, bot: Bot):
     await profile(bot, data=data, id_user=id_user, text='', keyboard=None)
 
 
-@router.message(F.text == LEXICON_keyboard["friend_button"][1])
+@router.message(F.text == LEXICON_keyboard["friend_button"][1], StateFilter(default_state))
 async def gift_list_secret_friend(message: Message):
     id_user = message.from_user.id
     id_friend = await db_manager.get_user_by_id(user_id=id_user)
     data = await db_manager.get_gift_list(user_id=id_friend.id_secret_friend)
-    print(data)
     if data.list == "СЮРПРИЗ":
         await message.answer(LEXICON["surprise"].format(gift_list=data.list))
     else:
         await message.answer(LEXICON_keyboard["gift_list_friend"].format(gift_list=data.list))
 
 
-@router.message(F.text == LEXICON_keyboard["friend_button"][2])
+@router.message(F.text == LEXICON_keyboard["friend_button"][2], StateFilter(default_state))
 async def gift_list_secret_friend(message: Message):
     id_user = message.from_user.id
     id_friend = await db_manager.get_user_by_id(user_id=id_user)
@@ -53,7 +55,7 @@ async def gift_list_secret_friend(message: Message):
                                                                          gifts=data.list))
 
 
-@router.message(F.text == LEXICON["menu"])
+@router.message(F.text == LEXICON["menu"], StateFilter(default_state))
 async def gift_list_secret_friend(message: Message):
     if (await db_manager.get_user_by_id(user_id=message.from_user.id)).creator_id:
         await message.answer('Хоу-хоу-хоу🎅', reply_markup=keyboard_creator.as_markup(resize_keyboard=True))
@@ -64,29 +66,13 @@ async def gift_list_secret_friend(message: Message):
 @router.callback_query(CallbackFactory.filter())
 async def gift_details(callback: CallbackQuery, callback_data: CallbackFactory):
     id_user = callback_data.user_id
-    index = callback_data.index
-    gift = (await db_manager.get_gift_list(user_id=id_user)).list.strip()
-    data = read_json(user_id=id_user)
-    # print(data[-1]['id'])
-    await callback.message.edit_text(f"Подробности о подарке {gift}:\n"
-                                  f"Дешевый вариант: \n" + LEXICON["general_list"].format(id=data[0]["id"],
-                                                                                          brand=data[0]["brand"],
-                                                                                          name=data[0]["name"],
-                                                                                          price=data[0]["price"],
-                                                                                          supplierRating=data[0][
-                                                                                              "supplierRating"],
-                                                                                          feedbacks=data[0][
-                                                                                              "feedbacks"]) +
-                                  f"\nДорогой вариант: \n" +
-                                  LEXICON["general_list"].format(id=data[-1]["id"],
-                                                                 brand=data[-1]["brand"],
-                                                                 name=data[-1]["name"],
-                                                                 price=data[-1]["price"],
-                                                                 supplierRating=data[-1]["supplierRating"],
-                                                                 feedbacks=data[-1]["feedbacks"]) +
-                                  f"\nДоступен в магазинах: All")
-    # message_text += f"Доступен в магазинах: {', '.join(gift['shops'])}")
-    # message_text = f"Подробности о подарке {gift['name']}:\n"
-    # message_text += f"Дешевый вариант: {gift['price']['дешевый']} рублей\n"
-    # message_text += f"Дорогой вариант: {gift['price']['дорогой']} рублей\n"
-    # message_text += f"Доступен в магазинах: {', '.join(gift['shops'])}"
+    gifts = await db_manager.get_generate_gift(user_id=id_user, name_gift=callback_data.gift)
+    for gift in gifts:
+        await callback.message.reply(f"Подробности о подарке {gift.name}:\n"
+                                     f"{gift.status} вариант: \n" +
+                                     LEXICON["general_list"].format(id=gift.id_gift,
+                                                                    brand=gift.brand,
+                                                                    name=gift.name,
+                                                                    price=gift.price,
+                                                                    supplierRating=gift.supplierRating,
+                                                                    feedbacks=gift.feedbacks))
