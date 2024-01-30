9da9f1c2a0caaf98a7068005443fe0cc92bf32ab
Denis Evtushok <evtushokdenis2004@gmail.com>
2024-01-30 12:29:13 +0300

    new commit

--- handlers/general/start_command.py
+++ handlers/general/start_command.py
@@ -4,16 +4,21 @@ from aiogram.fsm.state import default_state
 from aiogram.types import Message, CallbackQuery
 
 from LEXICON.lexicon import LEXICON
+from config_data.config import DATABASE_URL
+from database.requests import DatabaseManager
 from keyboards.general_keyboards import keyboard_start, read
 from filters.filter import IsPrivate
 
 
 router = Router()
 router.message.filter(IsPrivate())
+dsn = DATABASE_URL
+db_manager = DatabaseManager(dsn=dsn)
 
 
 @router.message(CommandStart(), StateFilter(default_state))
 async def start(message: Message):
+    # await db_manager.create_tables()
     await message.answer(text=LEXICON["start"], reply_markup=keyboard_start)
 
 
