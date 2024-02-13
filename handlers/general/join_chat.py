9bcf1c1be19ceffa0a153668a3713a91bb057c1b
Denis Evtushok <evtushokdenis2004@gmail.com>
2024-02-13 03:37:16 +0300

    Удаление игроков при удалении из группы

--- handlers/general/join_chat.py
+++ handlers/general/join_chat.py
@@ -17,8 +17,10 @@ db_manager = DatabaseManager(dsn=dsn)
 async def on_user_joined(message: Message, bot: Bot):
     new_user = message.new_chat_members[0]
     new_user_db = {'user_id': new_user.id, 'chat_id': message.chat.id}
-    user = await db_manager.get_user_by_id(user_id=new_user.id)
-    users = await db_manager.get_all_users(id_chat=user.chat_id)
+    # user = await db_manager.get_user_by_id(user_id=new_user.id)
+    # print(user, '--')
+    users = await db_manager.get_all_users(id_chat=new_user_db["chat_id"])
+    # print(users, '++')
     if len(users) >= 15:
         await bot.send_message(chat_id=users[0][1], text=LEXICON_Creator["many_players"])
     else:
@@ -31,17 +33,9 @@ async def on_user_joined(message: Message, bot: Bot):
 
 
 # удаляем юзера
-# @router.message(F.content_type == 'new_chat_members')
-# async def on_user_joined(message: Message, bot: Bot):
-#     new_user = message.new_chat_members[0]
-#     new_user_db = {'user_id': new_user.id, 'chat_id': message.chat.id}
-#     users = await db_manager.get_all_users()
-#     if len(users) == 15:
-#         await bot.send_message(chat_id=users[0][1], text=LEXICON_Creator["many_players"])
-#     else:
-#         await db_manager.add_user(new_user_db)
-#
-#         if new_user.username:
-#             await message.answer(LEXICON_user["user_start"].format(name=f'@{new_user.username}'))
-#         else:
-#             await message.answer(LEXICON_user["user_start"].format(name=new_user.first_name))
+@router.message(F.content_type == 'left_chat_member')
+async def on_user_joined(message: Message, bot: Bot):
+    # print(message.left_chat_member.id)
+    new_user = message.left_chat_member.id
+    await db_manager.delete_user(user_id=new_user)
+    await bot.send_message(chat_id=new_user, text=LEXICON_Creator["delete"])
