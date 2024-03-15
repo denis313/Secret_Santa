0a5ded2967b479b78a0ea0f19e2d8de9a84b8eae
Denis Evtushok <evtushokdenis2004@gmail.com>
2024-03-15 11:26:17 +0300

    new handlers

--- handlers/admin/for_creator.py
+++ handlers/admin/for_creator.py
@@ -1,13 +1,16 @@
 import logging
 
 from aiogram import F, Router, Bot
-from aiogram.types import Message
+from aiogram.filters import StateFilter
+from aiogram.fsm.state import default_state
+from aiogram.types import Message, CallbackQuery
 
 from LEXICON.lexicon import LEXICON_Creator, LEXICON_keyboard
 from config_data.config import DATABASE_URL
 from database.requests import DatabaseManager
 from filters.filter import IsPrivate, IsCreator
 from handlers.general.message_profile import profile
+from keyboards.general_keyboards import keyboard_stock
 from services.services import random_assignment
 
 logger = logging.getLogger(__name__)
@@ -51,3 +54,26 @@ async def stop_the_game(message: Message, bot: Bot):
 @router.message(F.text == LEXICON_keyboard["button"][5])
 async def buying_places_play(message: Message):
     print(message.from_user.id)
+
+
+@router.message(F.text == LEXICON_keyboard["button"][6])
+async def stock_gifts(message: Message):
+    await message.answer(LEXICON_Creator["stock"], reply_markup=keyboard_stock.as_markup())
+
+
+@router.callback_query(F.data == LEXICON_keyboard["stock"]["Бюджет игроков"], StateFilter(default_state))
+async def for_new_user(callback: CallbackQuery):
+    user = callback.from_user
+
+
+@router.callback_query(F.data == LEXICON_keyboard["stock"]["Подсчитать бюджет"], StateFilter(default_state))
+async def for_new_user(callback: CallbackQuery):
+    user = callback.from_user
+
+
+@router.callback_query(F.data == LEXICON_keyboard["stock"]["Обсудить в Группе"], StateFilter(default_state))
+async def for_new_user(callback: CallbackQuery, bot: Bot):
+    user = callback.from_user
+    data = await db_manager.get_user_by_id(user_id=user.id)
+    await callback.message.answer(text=LEXICON_Creator["stock_answer"])
+    await bot.send_message(chat_id=data.chat_id, text=LEXICON_Creator['stock_chat'])
